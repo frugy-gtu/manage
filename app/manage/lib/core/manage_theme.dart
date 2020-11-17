06d8bfbea1b654b44fa830af8f5964866aa71822
repelliuss <repelliuss@gmail.com>
2020-11-17 21:23:49 +0300

    Refactor ManageThemeMode with ThemeMode
    Well, Flutter already has it.

--- app/manage/lib/core/manage_theme.dart
+++ app/manage/lib/core/manage_theme.dart
@@ -1,33 +1,27 @@
 import 'package:flutter/material.dart';
 
-enum ManageThemeMode {
-  system,
-  light,
-  dark,
-}
-
 class ManageTheme {
-  static var _themeMode = ManageThemeMode.system;
+  static var _themeMode = ThemeMode.system;
 
   static final _light = ThemeData.light();
   static final _dark = ThemeData.dark();
 
   static get light {
     switch (_themeMode) {
-      case ManageThemeMode.system:
-      case ManageThemeMode.light:
+      case ThemeMode.system:
+      case ThemeMode.light:
         return _light;
-      case ManageThemeMode.dark:
+      case ThemeMode.dark:
         return _dark;
     }
   }
 
   static get dark {
     switch (_themeMode) {
-      case ManageThemeMode.system:
-      case ManageThemeMode.dark:
+      case ThemeMode.system:
+      case ThemeMode.dark:
         return _dark;
-      case ManageThemeMode.light:
+      case ThemeMode.light:
         return _light;
     }
   }
