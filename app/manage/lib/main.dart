04d41ca75b4f7a2fc1a124d6a53761fdf7b984fc
repelliuss <repelliuss@gmail.com>
2020-12-04 18:27:39 +0300

    Add Hive initialization

--- app/manage/lib/main.dart
+++ app/manage/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/widgets.dart' show runApp;
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'core/manage_app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter((await getApplicationDocumentsDirectory()).path);
   runApp(ManageApp());
 }
