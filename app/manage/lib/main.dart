b259187955995adca11d517f7674cebdd2158de8
repelliuss <repelliuss@gmail.com>
2020-12-04 23:35:08 +0300

    Fix Flutter not being initialized

--- app/manage/lib/main.dart
+++ app/manage/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart' show runApp;
+import 'package:flutter/widgets.dart' show WidgetsFlutterBinding, runApp;
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
@@ -6,6 +6,7 @@ import 'package:path_provider/path_provider.dart';
 import 'core/manage_app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter((await getApplicationDocumentsDirectory()).path);
   runApp(ManageApp());
 }
