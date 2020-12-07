d44a22dfabf9d3da61a357ece59095fd500e579c
repelliuss <repelliuss@gmail.com>
2020-12-07 18:06:30 +0300

    Rename ManageApp to App

---
+++ app/manage/lib/core/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart' hide Theme;
+import 'package:manage/core/team_home_page.dart';
+
+import 'theme.dart';
+import 'settings.dart';
+
+class App extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: Settings.listenable(['themeMode']),
+        builder: (context, box, widget) {
+          return MaterialApp(
+            title: 'Manage',
+            theme: Theme.light,
+            darkTheme: Theme.dark,
+            themeMode: Settings.themeMode,
+            home: TeamHomePage(),
+            debugShowCheckedModeBanner: false,
+          );
+        });
+  }
+}
