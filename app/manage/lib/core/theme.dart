8f626a26d729b060751c2e48798a69dd33855d2c
repelliuss <repelliuss@gmail.com>
2020-12-07 18:07:07 +0300

    Rename ManageTheme to Theme

---
+++ app/manage/lib/core/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Theme {
+  static final _light = ThemeData.light();
+  static final _dark = ThemeData.dark();
+
+  static get light => _light;
+
+  static get dark => _dark;
+}
