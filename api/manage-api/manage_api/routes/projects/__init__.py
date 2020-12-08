e4c7725a0d0366e725e1ea97bdd5968fe823a8d8
ismltpn <ismltpn@gmail.com>
2020-12-09 01:55:45 +0300

    Create response schemas for project routes

--- api/manage-api/manage_api/routes/projects/__init__.py
+++ api/manage-api/manage_api/routes/projects/__init__.py
@@ -1,8 +1,8 @@
 from flask_restx import Namespace, Resource
-from flask_accepts import accepts
+from flask_accepts import accepts, responds
 from flask_jwt_extended import jwt_required
 
-from .schemas import request as request_schemas
+from .schemas import request as request_schemas, response as response_schemas
 
 
 api = Namespace('projects', 'Project related routes')
@@ -11,6 +11,7 @@ api = Namespace('projects', 'Project related routes')
 @api.route('/')
 class Projects(Resource):
     @jwt_required
+    @responds(schema=response_schemas.Project, api=api, status_code=200)
     def get(self, **kwargs):
         raise NotImplementedError()
 
@@ -18,15 +19,18 @@ class Projects(Resource):
 @api.route('/<uuid:project_id>')
 class Project(Resource):
     @jwt_required
+    @responds(schema=response_schemas.Project, api=api, status_code=200)
     def get(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
     @accepts(schema=request_schemas.ProjectPutSchema, api=api)
+    @responds(schema=response_schemas.Project, api=api, status_code=200)
     def put(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
+    @responds(schema=response_schemas.Project, api=api, status_code=200)
     def delete(self, project_id, **kwargs):
         raise NotImplementedError()
 
@@ -34,20 +38,24 @@ class Project(Resource):
 @api.route('/<uuid:project_id>/tags')
 class ProjectTags(Resource):
     @jwt_required
+    @responds(schema=response_schemas.ProjectTag, api=api, status_code=200)
     def get(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
     @accepts(schema=request_schemas.ProjectTagsPostSchema, api=api)
+    @responds(schema=response_schemas.ProjectTag, api=api, status_code=201)
     def post(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
     @accepts(schema=request_schemas.ProjectTagsPutSchema, api=api)
+    @responds(schema=response_schemas.ProjectTag, api=api, status_code=201)
     def put(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
+    @responds(schema=response_schemas.ProjectTag, api=api, status_code=200)
     def delete(self, project_id, **kwargs):
         raise NotImplementedError()
 
@@ -55,20 +63,24 @@ class ProjectTags(Resource):
 @api.route('/<uuid:project_id>/states')
 class ProjectStates(Resource):
     @jwt_required
+    @responds(schema=response_schemas.ProjectState, api=api, status_code=200)
     def get(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
     @accepts(schema=request_schemas.ProjectStatesPostSchema, api=api)
+    @responds(schema=response_schemas.ProjectState, api=api, status_code=201)
     def post(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
     @accepts(schema=request_schemas.ProjectStatesPutSchema, api=api)
+    @responds(schema=response_schemas.ProjectState, api=api, status_code=201)
     def put(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
+    @responds(schema=response_schemas.ProjectState, api=api, status_code=200)
     def delete(self, project_id, **kwargs):
         raise NotImplementedError()
 
@@ -76,11 +88,13 @@ class ProjectStates(Resource):
 @api.route('/<uuid:project_id>/task_groups')
 class ProjectTaskGroups(Resource):
     @jwt_required
+    @responds(schema=response_schemas.TaskGroup, api=api, status_code=200)
     def get(self, project_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
     @accepts(schema=request_schemas.ProjectTaskGroupsPostSchema, api=api)
+    @responds(schema=response_schemas.TaskGroup, api=api, status_code=201)
     def post(self, project_id, **kwargs):
         raise NotImplementedError()
 
@@ -89,9 +103,11 @@ class ProjectTaskGroups(Resource):
 class ProjectTaskGroup(Resource):
     @jwt_required
     @accepts(schema=request_schemas.ProjectTaskGroupPutSchema, api=api)
+    @responds(schema=response_schemas.TaskGroup, api=api, status_code=200)
     def put(self, project_id, task_group_id, **kwargs):
         raise NotImplementedError()
 
     @jwt_required
+    @responds(schema=response_schemas.TaskGroup, api=api, status_code=200)
     def delete(self, project_id, task_group_id, **kwargs):
         raise NotImplementedError()
