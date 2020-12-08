6566c74ecba044c1f3119b2494308f16b2f1b820
ismltpn <ismltpn@gmail.com>
2020-12-08 19:51:14 +0300

    Create request schemas for task routes

--- api/manage-api/manage_api/routes/tasks/__init__.py
+++ api/manage-api/manage_api/routes/tasks/__init__.py
@@ -1,54 +1,75 @@
 from flask_restx import Namespace, Resource
+from flask_accepts import accepts
+from flask_jwt_extended import jwt_required
+
+from .schemas import request as request_schemas
 
 api = Namespace('tasks', 'Task related routes')
 
 
 @api.route('/')
 class Tasks(Resource):
+    @jwt_required
     def get(self, **kwargs):
         raise NotImplementedError()
 
+    @jwt_required
+    @accepts(schema=request_schemas.TasksPostSchema, api=api)
     def post(self, **kwargs):
         raise NotImplementedError()
 
 
 @api.route('/<uuid:task_id>')
 class Task(Resource):
+    @jwt_required
     def get(self, task_id, **kwargs):
         raise NotImplementedError()
 
+    @jwt_required
+    @accepts(schema=request_schemas.TaskPutSchema, api=api)
     def put(self, task_id, **kwargs):
         raise NotImplementedError()
 
+    @jwt_required
     def delete(self, task_id, **kwargs):
         raise NotImplementedError()
 
 
 @api.route('/<uuid:task_id>/state')
 class TaskState(Resource):
+    @jwt_required
+    @accepts(schema=request_schemas.TaskStatePutSchema, api=api)
     def put(self, task_id, **kwargs):
         raise NotImplementedError()
 
 
 @api.route('/<uuid:task_id>/tag')
 class TaskTag(Resource):
+    @jwt_required
+    @accepts(schema=request_schemas.TaskTagPutSchema, api=api)
     def put(self, task_id, **kwargs):
         raise NotImplementedError()
 
 
 @api.route('/<uuid:task_id>/logs')
 class TaskLogs(Resource):
+    @jwt_required
     def get(self, task_id, **kwargs):
         raise NotImplementedError()
 
+    @jwt_required
+    @accepts(schema=request_schemas.TimeLogsPostSchema, api=api)
     def post(self, task_id, **kwargs):
         raise NotImplementedError()
 
 
 @api.route('/<uuid:task_id>/logs/<uuid:log_id>')
 class TaskLog(Resource):
+    @jwt_required
+    @accepts(schema=request_schemas.TimeLogPutSchema, api=api)
     def put(self, task_id, **kwargs):
         raise NotImplementedError()
 
+    @jwt_required
     def delete(self, task_id, **kwargs):
         raise NotImplementedError()
