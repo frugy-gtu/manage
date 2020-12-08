60903fc14d1062f3060bbc6cd3f0fe1d21383e36
repelliuss <repelliuss@gmail.com>
2020-12-08 03:33:29 +0300

    Update returned theme logic

--- app/manage/lib/core/theme.dart
+++ app/manage/lib/core/theme.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:manage/core/settings.dart';
 
 class Theme {
   //TODO: Select a font familty.
   static final _light = (() {
-      ColorScheme colorScheme = ColorScheme(
-        brightness: Brightness.light,
-        primary: Colors.white,
-        onPrimary: Colors.black,
-        primaryVariant: Colors.white54,
-        secondary: Colors.black,
-        onSecondary: Colors.white,
-        secondaryVariant: Colors.black54,
-        background: Colors.white,
-        onBackground: Colors.black,
-        surface: Colors.black,
-        onSurface: Colors.white,
-        error: Colors.red,
-        onError: Colors.white,
-      );
+    ColorScheme colorScheme = ColorScheme(
+      brightness: Brightness.light,
+      primary: Colors.white,
+      onPrimary: Colors.black,
+      primaryVariant: Colors.white54,
+      secondary: Colors.black,
+      onSecondary: Colors.white,
+      secondaryVariant: Colors.black54,
+      background: Colors.white,
+      onBackground: Colors.black,
+      surface: Colors.black,
+      onSurface: Colors.white,
+      error: Colors.red,
+      onError: Colors.white,
+    );
 
-      //TODO: Complete left styles.
-      TextTheme textTheme = TextTheme(
-        bodyText1: TextStyle(
-          color: Colors.black,
-        ),
-        button: TextStyle(
-          color: Colors.white,
-        ),
-      );
+    //TODO: Complete left styles.
+    TextTheme textTheme = TextTheme(
+      bodyText1: TextStyle(
+        color: Colors.black,
+      ),
+      button: TextStyle(
+        color: Colors.white,
+      ),
+    );
 
     return ThemeData.from(
       colorScheme: colorScheme,
@@ -44,7 +45,23 @@ class Theme {
 
   static final _dark = ThemeData.dark();
 
-  static get light => _light;
+  static get light {
+    switch (Settings.themeMode) {
+      case ThemeMode.system:
+      case ThemeMode.light:
+        return _light;
+      case ThemeMode.dark:
+        return _dark;
+    }
+  }
 
-  static get dark => _dark;
+  static get dark {
+    switch (Settings.themeMode) {
+      case ThemeMode.system:
+      case ThemeMode.dark:
+        return _dark;
+      case ThemeMode.light:
+        return _light;
+    }
+  }
 }
