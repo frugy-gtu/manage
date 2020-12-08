9c1a579d0d30f29d1c6e24411ad366da61cdc8f4
ismltpn <ismltpn@gmail.com>
2020-12-08 19:43:38 +0300

    Create request schemas for user routes

--- api/manage-api/manage_api/routes/users/__init__.py
+++ api/manage-api/manage_api/routes/users/__init__.py
@@ -1,21 +1,27 @@
 from flask_restx import Namespace, Resource
+from flask_accepts import accepts
+
+from .schemas import request as request_schemas
 
 api = Namespace('users', 'User related routes')
 
 
 @api.route('/login')
 class Login(Resource):
+    @accepts(schema=request_schemas.LoginSchema, api=api)
     def post(self, **kwargs):
         raise NotImplementedError()
 
 
 @api.route('/signup')
 class Signup(Resource):
+    @accepts(schema=request_schemas.SignupSchema, api=api)
     def post(self, **kwargs):
         raise NotImplementedError()
 
 
 @api.route('/activate')
 class Activate(Resource):
+    @accepts(schema=request_schemas.ActivateSchema, api=api)
     def post(self, **kwargs):
         raise NotImplementedError()
