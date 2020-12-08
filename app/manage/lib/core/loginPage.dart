b4fcfdc9e5695b62db5942a560a648deb78f49ea
Yusuf Fatih Şişman <62207089+YusufFatihSisman@users.noreply.github.com>
2020-12-08 18:36:54 +0200

    Login and Register Screens

---
+++ app/manage/lib/core/loginPage.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  TextEditingController _mailCont;
+  TextEditingController _passCont;
+  bool _validMail = true;
+  bool _validPass = true;
+  bool _userExist = true;
+
+  void initState(){
+    super.initState();
+    _mailCont = TextEditingController();
+    _passCont = TextEditingController();
+  }
+
+  void dispose(){
+    _mailCont.dispose();
+    _passCont.dispose();
+    super.dispose();
+  }
+
+  Future<void> login(String mail, String password) async{
+    if(await DBProvider.db.login(mail, password)){
+      setState(() {
+        _userExist = true;
+      });
+    }else{
+      setState(() {
+        _userExist = false;
+      });
+    }
+  }
+
+  Future<User> getUser(String mail, String password) async{
+    return await DBProvider.db.getUser(mail, password);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 40.0,),
+              TextField(
+                controller: _mailCont,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'email',
+                  errorText: _validMail ? null : 'You must fill here',
+                ),
+              ),
+              SizedBox(height: 10.0,),
+              TextField(
+                obscureText: true,
+                controller: _passCont,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'password',
+                  errorText: _validPass ? null : 'You must fill here',
+                ),
+              ),
+              SizedBox(height: 5.0,),
+              Center(
+                child: Text(
+                  _userExist ? '' : 'Wrong information',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              SizedBox(height: 5.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  RaisedButton(
+                    child: Text('Login'),
+                    onPressed: (){
+                      setState(() {
+                        _mailCont.text.isEmpty ? _validMail = false : _validMail = true;
+                        _passCont.text.isEmpty ? _validPass = false : _validPass = true;
+                      });
+                      if(_validMail==true && _validPass==true){
+                        login(_mailCont.text, _passCont.text);
+                        if(_userExist){
+                          Navigator.pop();
+                        }
+                      }
+                    }      
+                  ),
+                  SizedBox(width: 5.0,),
+                  RaisedButton(
+                    child: Text('Sign up'),
+                    onPressed: (){
+                      setState(() {
+                        _mailCont.clear();
+                        _passCont.clear();
+                      });
+                      Navigator.pushNamed(context, '/registerPage');
+                    },
+                  )  
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ app/manage/lib/core/registerPage.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+
+  TextEditingController _fNameCont;
+  TextEditingController _lNameCont;
+  TextEditingController _mailCont;
+  TextEditingController _passCont;
+  bool _validFName = true;
+  bool _validLName = true;
+  bool _validMail = true;
+  bool _validPass = true;
+  bool _registered = true;
+
+  void initState(){
+    super.initState();
+    _fNameCont = TextEditingController();
+    _lNameCont = TextEditingController();
+    _mailCont = TextEditingController();
+    _passCont = TextEditingController();
+  }
+
+  void dispose(){
+    _fNameCont.dispose();
+    _lNameCont.dispose();
+    _mailCont.dispose();
+    _passCont.dispose();
+    super.dispose();
+  }
+
+  Future<void> register(String mail, String password) async{
+    if(await DBProvider.db.userExist(mail)){
+      setState(() {
+        _registered = false;
+      });
+    }else{
+      setState(() {
+        _registered = true;
+      });
+      DBProvider.db.insertUser(User(fName: _fNameCont.text, lName: _lNameCont.text, mail: _mailCont.text, password: _passCont.text));
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(title: Text('Sign up'),),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 40.0,),
+              TextField(
+                controller: _fNameCont,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'first name',
+                  errorText: _validFName ? null : 'You must fill here',
+                ),
+              ),
+              SizedBox(height: 10.0,),
+              TextField(
+                controller: _lNameCont,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'last name',
+                  errorText: _validLName ? null : 'You must fill here',
+                ),
+              ),
+              SizedBox(height: 10.0,),
+              TextField(
+                controller: _mailCont,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'email',
+                  errorText: _validMail ? null : 'You must fill here',
+                ),
+              ),
+              SizedBox(height: 10.0,),
+              TextField(
+                controller: _passCont,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'password',
+                  errorText: _validPass ? null : 'You must fill here',
+                ),
+              ),
+              SizedBox(height: 10.0,),
+              Center(
+                child: Text(
+                  _registered ? '' : 'Mail address is already used',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              RaisedButton(
+                child: Text('Sign up'),
+                onPressed: (){
+                  setState(() {
+                    _fNameCont.text.isEmpty ? _validFName = false : _validFName = true;
+                    _lNameCont.text.isEmpty ? _validLName = false : _validLName = true;
+                    _mailCont.text.isEmpty ? _validMail = false : _validMail = true;
+                    _passCont.text.isEmpty ? _validPass = false : _validPass = true;
+                  });
+                  if(_validFName==true && _validLName==true && _validMail==true && _validPass==true){
+                    register(_mailCont.text, _passCont.text);
+                  }
+                }, 
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
