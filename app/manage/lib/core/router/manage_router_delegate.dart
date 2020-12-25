56a92f8fb863f7993881b98d3571ceb7a6f356f0
repelliuss <repelliuss@gmail.com>
2020-12-25 23:32:47 +0300

    Update RouteDelegator with new state

--- app/manage/lib/core/router/manage_router_delegate.dart
+++ app/manage/lib/core/router/manage_router_delegate.dart
@@ -1,60 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:manage/core/router/team_route_path.dart';
+import 'package:manage/core/router/manage_route_path.dart';
+import 'package:manage/core/router/manage_route_state.dart';
+import 'package:manage/core/router/manage_teams_path.dart';
+import 'package:manage/core/router/manage_unknown_path.dart';
 import 'package:manage/core/screens/teams_screen.dart';
+import 'package:manage/core/team.dart';
 
-import '../team.dart';
+import 'manage_team_path.dart';
 
 class ManageRouterDelegate extends RouterDelegate<ManageRoutePath>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<ManageRoutePath> {
   final GlobalKey<NavigatorState> _navigatorKey;
 
-  Team _currentTeam;
-  bool _show404 = false;
-  List<Team> teams = [
-    Team('User Team', '0'),
-  ];
+  final ManageRouteState state = ManageRouteState();
 
-  ManageRouterDelegate() : _navigatorKey = GlobalKey<NavigatorState>();
-
-  void _handleTeamTapped(Team team) {
-    _currentTeam = team;
-    notifyListeners();
+  ManageRouterDelegate() : _navigatorKey = GlobalKey<NavigatorState>() {
+    state.addListener(notifyListeners);
   }
 
   @override
   Widget build(BuildContext context) {
     return Navigator(
       key: navigatorKey,
-      pages: [
-        MaterialPage(
-          key: ValueKey('TeamsPage'),
-          child: TeamsScreen(
-            teams: teams,
-            onTapped: _handleTeamTapped,
-          ),
-        ),
-        if (_show404)
-          MaterialPage(
-            key: ValueKey('UnknownPage'),
-            child: Scaffold(),
-          )
-        else if (_currentTeam != null)
-          MaterialPage(
-              key: ValueKey(_currentTeam),
-              child: Scaffold(
-                appBar: AppBar(
-                  title: Text(_currentTeam.name),
-                ),
-              )),
-      ],
+      pages: _buildPages(),
       onPopPage: (route, result) {
+        print('I popped page');
         if (!route.didPop(result)) {
           return false;
         }
-        _currentTeam = null;
-        _show404 = false;
 
-        notifyListeners();
+        state.update(ManageRoute.teams);
 
         return true;
       },
@@ -65,36 +40,43 @@ class ManageRouterDelegate extends RouterDelegate<ManageRoutePath>
   GlobalKey<NavigatorState> get navigatorKey => _navigatorKey;
 
   @override
-  Future<void> setNewRoutePath(ManageRoutePath configuration) async {
-    if(configuration.isUnknown) {
-      _currentTeam = null;
-      _show404 = true;
-      return;
+  Future<void> setNewRoutePath(ManageRoutePath path) async {
+    print('I set new route path');
+    if (path is ManageTeamsPath) {
+      state.update(ManageRoute.teams);
+    } else if (path is ManageTeamPath) {
+      state.update(ManageRoute.team, team: Team.fromId(path.id));
+    } else if (path is ManageUnknownPath) {
+      state.update(ManageRoute.unknown);
     }
-
-    if(configuration.isDetailsPage) {
-      int id = int.parse(configuration.id);
-      if(id < 0 || id >= teams.length) {
-        _show404 = true;
-        return;
-      }
-
-      _currentTeam = teams[id];
-    }
-    else {
-      _currentTeam = null;
-    }
-
-    _show404 = false;
   }
 
-  ManageRoutePath get currentConfiguration {
-    if (_show404) {
-      return ManageRoutePath.unknown();
+  List<Page<dynamic>> _buildPages() {
+    List<Page<dynamic>> pages = [
+      MaterialPage(
+        key: ValueKey('TeamsPage'),
+        child: TeamsScreen(
+          onTapped: _handleTeamTapped,
+        ),
+      ),
+    ];
+
+    if (state.route == ManageRoute.team) {
+      pages.add(MaterialPage(
+          key: ValueKey('TeamPage'),
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(state.team.name),
+            ),
+          )
+        )
+      );
     }
 
-    return _currentTeam == null
-        ? ManageRoutePath.home()
-        : ManageRoutePath.details(_currentTeam.id);
+    return pages;
+  }
+
+  void _handleTeamTapped(Team team) {
+    state.update(ManageRoute.team, team: team);
   }
 }
