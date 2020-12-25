43cd625e76df301f9cce6179d399d9485cf53683
repelliuss <repelliuss@gmail.com>
2020-12-25 17:48:57 +0300

    Init auth caching

--- app/manage/lib/main.dart
+++ app/manage/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart' show WidgetsFlutterBinding, runApp;
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:manage/core/auth.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'core/app.dart';
@@ -18,5 +19,6 @@ void main() async {
   };
   await Hive.initFlutter((await getApplicationDocumentsDirectory()).path);
   await Settings.init();
+  await Auth.init();
   runApp(App());
 }
