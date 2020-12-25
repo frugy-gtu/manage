12ec779f4e7874ea2dd98d0fef1731fd094df568
repelliuss <repelliuss@gmail.com>
2020-12-25 17:46:24 +0300

    Add route state

---
+++ app/manage/lib/core/router/manage_route_state.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../team.dart';
+
+class ManageRouteState extends ChangeNotifier {
+  ManageRoute _currentRoute;
+  Team _currentTeam;
+
+  List<Team> teams = [
+    Team('User Team', '0'),
+  ];
+
+  ManageRouteState() : _currentRoute = ManageRoute.teams;
+
+  ManageRoute get currentRoute => _currentRoute;
+
+  Team get currentTeam => _currentTeam;
+
+  void update(ManageRoute route, {Team team}) {
+    assert(route != null);
+
+    if(route == ManageRoute.team) {
+      assert(team != null);
+      _currentTeam = team;
+    }
+
+    _currentRoute = route;
+
+    notifyListeners();
+  }
+}
+
+enum ManageRoute {
+  login,
+  signup,
+  teams,
+  team,
+  team_creation,
+  unknown,
+}
