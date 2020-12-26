95292b60dca8ff2b066061adee9d4ba42b0f1bb5
ismltpn <ismltpn@gmail.com>
2020-12-27 00:57:38 +0300

    Create team get tests

--- api/manage-api/tests/test_team_routes.py
+++ api/manage-api/tests/test_team_routes.py
@@ -96,3 +96,71 @@ def test_teams_get_only_current_users_teams(flask_test_client):
     assert team2['id'] == team_2['id']
     assert team2['name'] == team_2['name']
 
+
+def test_team_get_without_jwt(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    response: Response = flask_test_client.get(f'/teams/{team_1["id"]}')
+    assert response.status_code == 401
+
+
+def test_team_get_with_jwt(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    access_token = user.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    response: Response = flask_test_client.get(
+        f'/teams/{team_1["id"]}', headers=headers
+    )
+    data = response.get_json()
+    assert response.status_code == 200
+    assert data['id'] == team_1['id']
+    assert data['name'] == team_data_1['name']
+    assert data['abbreviation'] == team_data_1['abbreviation']
+
+
+def test_team_get_with_jwt_not_associated_user(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    user_data_2 = {
+        'username': 'test_user2',
+        'email': 'test2@user.com',
+        'password': '123123asd',
+    }
+    user2 = db_services.UserService.create(user_data_2)
+    access_token = user.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user2['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    response: Response = flask_test_client.get(
+        f'/teams/{team_1["id"]}', headers=headers
+    )
+    assert response.status_code == 401
