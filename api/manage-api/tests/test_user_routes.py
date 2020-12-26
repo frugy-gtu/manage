5031efc6705f0eaa82050be5d19d6bb5ed840a25
ismltpn <ismltpn@gmail.com>
2020-12-26 16:31:29 +0300

    Create tests for user signup

--- api/manage-api/tests/test_user_routes.py
+++ api/manage-api/tests/test_user_routes.py
@@ -1,5 +1,6 @@
 from flask import Response
 from flask.testing import FlaskClient
+from manage_api.db import models as db_models
 from manage_api.db import services as db_services
 
 
@@ -61,3 +62,83 @@ def test_user_login_post_email_password(flask_test_client: FlaskClient):
         assert 'access_token' in data
     finally:
         user.delete()
+
+
+def test_user_signup_get(flask_test_client):
+    response: Response = flask_test_client.get('/users/signup')
+    assert response.status_code == 405
+
+
+def test_user_signup_delete(flask_test_client):
+    response: Response = flask_test_client.delete('/users/signup')
+    assert response.status_code == 405
+
+
+def test_user_signup_put(flask_test_client):
+    response: Response = flask_test_client.put('/users/signup')
+    assert response.status_code == 405
+
+
+def test_user_signup_post_email_username_password(flask_test_client):
+    try:
+        user_data = {
+            'username': '_testuser_',
+            'email': 'test@user.com',
+            'password': '1234567',
+        }
+        response: Response = flask_test_client.post('/users/signup', json=user_data)
+        data = response.get_json()
+        assert response.status_code == 201
+        assert data['username'] == user_data['username']
+        assert data['email'] == user_data['email']
+        user = db_models.User.query.get(data['id'])
+        assert user is not None
+        assert user.username == user_data['username']
+        assert user.email == user_data['email']
+    finally:
+        db_models.User.query.delete()
+        db_models.db.session.commit()
+
+
+def test_user_signup_post_used_email_username_password(flask_test_client):
+    try:
+        user_data_1 = {
+            'username': '_testuser1_',
+            'email': 'test1@user.com',
+            'password': '1234567',
+        }
+        user_data_2 = {
+            'username': '_testuser2_',
+            'email': 'test1@user.com',
+            'password': '1234567',
+        }
+        db_services.UserService.create(user_data_1)
+        response: Response = flask_test_client.post('/users/signup', json=user_data_2)
+        data = response.get_json()
+        assert response.status_code == 400
+        assert db_models.User.query.count() == 1
+    finally:
+        db_models.User.query.delete()
+        db_models.db.session.commit()
+
+
+def test_user_signup_post_email_used_username_password(flask_test_client):
+    try:
+        user_data_1 = {
+            'username': '_testuser1_',
+            'email': 'test1@user.com',
+            'password': '1234567',
+        }
+        user_data_2 = {
+            'username': '_testuser1_',
+            'email': 'test2@user.com',
+            'password': '1234567',
+        }
+        db_services.UserService.create(user_data_1)
+        response: Response = flask_test_client.post('/users/signup', json=user_data_2)
+        data = response.get_json()
+        assert response.status_code == 400
+        assert db_models.User.query.count() == 1
+    finally:
+        db_models.User.query.delete()
+        db_models.db.session.commit()
