3c621a9a9122bc203f76d1cf572d98fc12f35fcc
ismltpn <ismltpn@gmail.com>
2020-12-27 02:03:25 +0300

    Change the cascade behaviour of user_teams table

--- api/manage-api/manage_api/db/models/user_teams.py
+++ api/manage-api/manage_api/db/models/user_teams.py
@@ -3,5 +3,9 @@ from .db import db, UUID
 
 
 class UserTeams(Base):
-    team_id = db.Column(UUID(as_uuid=True), db.ForeignKey('team.id'), nullable=False)
-    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
+    team_id = db.Column(
+        UUID(as_uuid=True), db.ForeignKey('team.id', ondelete='CASCADE'), nullable=False
+    )
+    user_id = db.Column(
+        UUID(as_uuid=True), db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False
+    )
