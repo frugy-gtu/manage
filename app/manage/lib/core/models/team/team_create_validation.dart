6dce00d45224697ecb3b5998539826d8c42e9c04
repelliuss <repelliuss@gmail.com>
2020-12-26 23:31:06 +0300

    Add abbreviation calculator for team name

--- app/manage/lib/core/models/team/team_create_validation.dart
+++ app/manage/lib/core/models/team/team_create_validation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:manage/core/models/validation_item.dart';
 
 class TeamCreateValidation extends ChangeNotifier {
@@ -10,11 +11,13 @@ class TeamCreateValidation extends ChangeNotifier {
   ValidationItem get abbrv => _abbrv;
 
   void updateName(String value) {
-    if(value.length < 3 && value.length > 0) {
+    if(value.isNotEmpty && value.length < 3) {
       _name = ValidationItem(null, 'Must be at least 3 characters');
+      _abbrv = ValidationItem(null, null);
     }
     else {
       _name = ValidationItem(value, null);
+      _abbrv = ValidationItem(_calculateAbbrv(value), null);
     }
 
     notifyListeners();
@@ -28,5 +31,30 @@ class TeamCreateValidation extends ChangeNotifier {
     _abbrv = ValidationItem(value, null);
 
     notifyListeners();
+  }
+
+  String _calculateAbbrv(String name) {
+    String abbrv = name[0];
+    int index = 0;
+    if(name.length < 4) {
+      abbrv = name;
+    }
+    else if(name.contains(' ')) {
+      while((index = name.indexOf(' ', index)) != -1 && abbrv.length < 4) {
+        if(index < name.length)
+          abbrv += name[index+1];
+      }
+    }
+    else if(name.length % 2 == 0) {
+      while(index < name.length && index < 5) {
+        index += 2;
+        abbrv += name[index];
+      }
+    }
+    else {
+      abbrv += name[(name.length / 2).ceil()];
+    }
+
+    return abbrv;
   }
 }
