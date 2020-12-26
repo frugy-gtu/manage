6bd916623fad6aaf38558971c4875ddef8554d5a
ismltpn <ismltpn@gmail.com>
2020-12-27 02:05:24 +0300

    Create team delete tests

--- api/manage-api/tests/test_team_routes.py
+++ api/manage-api/tests/test_team_routes.py
@@ -338,3 +338,78 @@ def test_team_put_with_jwt_not_manager(flask_test_client):
     assert team.name != team_update_data['name']
     assert team.name == team_data_1['name']
 
+
+def test_team_delete_without_jwt(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    response: Response = flask_test_client.delete(
+        f'/teams/{team_1["id"]}',
+    )
+    assert response.status_code == 401
+
+
+def test_team_delete_with_jwt(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    access_token = user.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    response: Response = flask_test_client.delete(
+        f'/teams/{team_1["id"]}', headers=headers
+    )
+    data = response.get_json()
+    assert response.status_code == 200
+    assert data['id'] == team_1['id']
+    assert data['name'] == team_1['name']
+    assert data['abbreviation'] == team_1['abbreviation']
+    team = db_models.Team.query.get(team_1['id'])
+    assert team is None
+
+
+def test_team_delete_with_jwt_not_manager(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    access_token = user.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    user_data_2 = {
+        'username': 'test_user2',
+        'email': 'test2@user.com',
+        'password': '123123asd',
+    }
+    user2 = db_services.UserService.create(user_data_2)
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user2['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    response: Response = flask_test_client.delete(
+        f'/teams/{team_1["id"]}', headers=headers
+    )
+    data = response.get_json()
+    assert response.status_code == 401
+    team = db_models.Team.query.get(team_1['id'])
+    assert team is not None
