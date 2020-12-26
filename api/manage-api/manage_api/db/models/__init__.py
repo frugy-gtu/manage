3bd31df62abe2f98ab896967870336c214e11a28
ismltpn <ismltpn@gmail.com>
2020-12-26 21:17:17 +0300

    Create database model for teams

--- api/manage-api/manage_api/db/models/__init__.py
+++ api/manage-api/manage_api/db/models/__init__.py
@@ -1,9 +1,12 @@
 from .db import db, update_db
+from .team import Team, TeamSchema
 from .user import User, UserSchema
 
 __all__ = [
     'db',
     'update_db',
+    'Team',
+    'TeamSchema',
     'User',
     'UserSchema',
 ]
