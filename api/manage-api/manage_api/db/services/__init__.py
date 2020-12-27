4f7c85bae637af468cf047fb30dc399446d5ad03
ismltpn <ismltpn@gmail.com>
2020-12-27 13:50:32 +0300

    Create db service for task group

--- api/manage-api/manage_api/db/services/__init__.py
+++ api/manage-api/manage_api/db/services/__init__.py
@@ -1,9 +1,11 @@
 from .project import ProjectService
+from .task_group import TaskGroupService
 from .team import TeamService
 from .user import UserService
 
 __all__ = [
     'ProjectService',
+    'TaskGroupService',
     'TeamService',
     'UserService',
 ]
