92a551bb5e299a3371aa3fe156c7bb3ba84673d9
ismltpn <ismltpn@gmail.com>
2020-12-27 06:21:21 +0300

    Fix team projects get test

--- api/manage-api/tests/test_team_routes.py
+++ api/manage-api/tests/test_team_routes.py
@@ -494,7 +494,7 @@ def test_team_projects_get_with_jwt(flask_test_client):
     assert project2['name'] == project_2['name']
 
 
-def test_team_projects_get_with_jwt_cuurent_teams_project(flask_test_client):
+def test_team_projects_get_with_jwt_current_teams_project(flask_test_client):
     user_data = {
         'username': 'test_user',
         'email': 'test@user.com',
@@ -503,28 +503,31 @@ def test_team_projects_get_with_jwt_cuurent_teams_project(flask_test_client):
     user = db_services.UserService.create(user_data)
     access_token = user.create_access_token()
     headers = {'Authorization': f'Bearer {access_token}'}
-    team_data_1 = {
+    team_1_data = {
         'name': 'test_team_1',
         'abbreviation': 'tt1',
         'user_id': user['id'],
     }
-    team_1 = db_services.TeamService.create(team_data_1).dump()
+    team_2_data = {
+        'name': 'test_team_2',
+        'abbreviation': 'tt2',
+        'user_id': user['id'],
+    }
+    team_1 = db_services.TeamService.create(team_1_data).dump()
+    team_2 = db_services.TeamService.create(team_2_data).dump()
     project_data_1 = {'name': 'test_project_1', 'team_id': team_1['id']}
-    project_data_2 = {'name': 'test_project_1', 'team_id': team_1['id']}
+    project_data_2 = {'name': 'test_project_2', 'team_id': team_2['id']}
     project_1 = db_services.ProjectService.create(project_data_1).dump()
-    project_2 = db_services.ProjectService.create(project_data_2).dump()
+    _ = db_services.ProjectService.create(project_data_2).dump()
     response: Response = flask_test_client.get(
         f'/teams/{team_1["id"]}/projects', headers=headers
     )
     data = response.get_json()
     assert response.status_code == 200
-    assert len(data) == 2
-    project1 = data[1]
-    project2 = data[0]
-    assert project1['id'] == project_1['id']
-    assert project1['name'] == project_1['name']
-    assert project2['id'] == project_2['id']
-    assert project2['name'] == project_2['name']
+    assert len(data) == 1
+    project = data[0]
+    assert project['id'] == project_1['id']
+    assert project['name'] == project_1['name']
 
 
 def test_team_projects_post_without_jwt(flask_test_client):
