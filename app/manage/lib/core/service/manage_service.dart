778048dfe8c782f652cbe0fe303ed23388d6bd49
repelliuss <repelliuss@gmail.com>
2020-12-27 22:20:26 +0300

    Add manage service

---
+++ app/manage/lib/core/service/manage_service.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:manage/core/cache/auth.dart';
+
+String _baseUrl = 'http://ec2-52-15-131-192.us-east-2.compute.amazonaws.com';
+
+Future<Response> post(String url,
+    {Map<String, dynamic> data, Map<String, dynamic> queryParameters}) async {
+  try {
+    return await Dio().post(_baseUrl + url,
+        data: data,
+        queryParameters: queryParameters,
+        options: Options(
+          headers: <String, dynamic>{
+            'Authorization': 'Bearer ' + Auth.accessToken
+          },
+        ));
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
