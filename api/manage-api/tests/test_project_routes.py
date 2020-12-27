5b7e317b47d6313ef5a502751f7102062fb0eea4
ismltpn <ismltpn@gmail.com>
2020-12-27 06:39:22 +0300

    Create project put tests

--- api/manage-api/tests/test_project_routes.py
+++ api/manage-api/tests/test_project_routes.py
@@ -205,3 +205,97 @@ def test_projects_get_with_jwt_not_associated_user(flask_test_client):
         f'/projects/{project_1["id"]}', headers=headers
     )
     assert response.status_code == 401
+
+
+def test_projects_put_without_jwt(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    project_data_1 = {'name': 'test_project_1', 'team_id': team_1['id']}
+    project_1 = db_services.ProjectService.create(project_data_1).dump()
+    project_update_data = {
+        'name': 'test_project_new_name',
+    }
+    response: Response = flask_test_client.put(
+        f'/projects/{project_1["id"]}',
+        json=project_update_data,
+    )
+    assert response.status_code == 401
+
+
+def test_projects_put_with_jwt(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    access_token = user.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    project_data_1 = {'name': 'test_project_1', 'team_id': team_1['id']}
+    project_1 = db_services.ProjectService.create(project_data_1).dump()
+    project_update_data = {
+        'name': 'test_project_new_name',
+    }
+    response: Response = flask_test_client.put(
+        f'/projects/{project_1["id"]}', json=project_update_data, headers=headers
+    )
+    data = response.get_json()
+    assert response.status_code == 200
+    assert data['id'] == project_1['id']
+    assert data['name'] == project_update_data['name']
+    project = db_models.Project.query.get(data['id'])
+    assert project is not None
+    assert project.name == project_update_data['name']
+
+
+def test_projects_put_with_jwt_not_manager(flask_test_client):
+    user_data = {
+        'username': 'test_user',
+        'email': 'test@user.com',
+        'password': '123123asd',
+    }
+    user = db_services.UserService.create(user_data)
+    access_token = user.create_access_token()
+    user_data_2 = {
+        'username': 'test_user2',
+        'email': 'test2@user.com',
+        'password': '123123asd',
+    }
+    user2 = db_services.UserService.create(user_data_2)
+    headers = {'Authorization': f'Bearer {access_token}'}
+    team_data_1 = {
+        'name': 'test_team_1',
+        'abbreviation': 'tt1',
+        'user_id': user2['id'],
+    }
+    team_1 = db_services.TeamService.create(team_data_1).dump()
+    project_data_1 = {'name': 'test_project_1', 'team_id': team_1['id']}
+    project_1 = db_services.ProjectService.create(project_data_1).dump()
+    project_update_data = {
+        'name': 'test_project_new_name',
+    }
+    response: Response = flask_test_client.put(
+        f'/projects/{project_1["id"]}', json=project_update_data, headers=headers
+    )
+    _ = response.get_json()
+    assert response.status_code == 401
+    project = db_models.Project.query.get(project_1['id'])
+    assert project is not None
+    assert project.name != project_update_data['name']
+    assert project.name == project_data_1['name']
