53c6c09b2873610b96efded162ca477edca3ebbc
Yusuf Fatih Şişman <62207089+YusufFatihSisman@users.noreply.github.com>
2020-12-27 21:27:24 +0200

    update
    Unused package is deleted.

--- app/manage/lib/core/screens/task_details_screen.dart
+++ app/manage/lib/core/screens/task_details_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sw_project/temps/userProfile.dart';
 import 'userPage.dart';
 
 import 'temps/task.dart';
