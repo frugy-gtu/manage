ddcea99c2e6ee8eeaf5431080ef306f2df78b0fb
ismltpn <ismltpn@gmail.com>
2020-12-27 15:30:57 +0300

    Create db model for task

--- api/manage-api/manage_api/db/models/__init__.py
+++ api/manage-api/manage_api/db/models/__init__.py
@@ -1,5 +1,6 @@
 from .db import db, update_db
 from .project import Project, ProjectSchema
+from .task import Task, TaskSchema
 from .task_group import TaskGroup, TaskGroupSchema
 from .team import Team, TeamSchema
 from .user import User, UserSchema
@@ -10,6 +11,8 @@ __all__ = [
     'update_db',
     'Project',
     'ProjectSchema',
+    'Task',
+    'TaskSchema',
     'TaskGroup',
     'TaskGroupSchema',
     'Team',
