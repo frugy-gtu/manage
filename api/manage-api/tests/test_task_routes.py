ca40b977336f11158f046497edb479873692ac0d
ismltpn <ismltpn@gmail.com>
2020-12-27 18:02:58 +0300

    Create tests for task routes

--- api/manage-api/tests/test_task_routes.py
+++ api/manage-api/tests/test_task_routes.py
@@ -199,3 +199,204 @@ def test_tasks_post_with_jwt_not_associated_user(flask_test_client):
     data = response.get_json()
     assert response.status_code == 401
     assert db_models.Task.query.count() == 0
+
+
+def test_task_get_without_jwt(flask_test_client):
+    user, project = create_user_and_project()
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data)
+    response: Response = flask_test_client.get(
+        f'/tasks/{task_1["id"]}',
+    )
+    assert response.status_code == 401
+
+
+def test_task_get_with_jwt(flask_test_client):
+    user, project = create_user_and_project()
+    access_token = user.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data).dump()
+    response: Response = flask_test_client.get(
+        f'/tasks/{task_1["id"]}',
+        headers=headers,
+    )
+    data = response.get_json()
+    assert response.status_code == 200
+    assert data['id'] == task_1['id']
+    assert data['name'] == task_1['name']
+
+
+def test_task_get_with_jwt_not_associated_user(flask_test_client):
+    user, project = create_user_and_project()
+    user2 = create_user(2)
+    access_token = user2.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data)
+    response: Response = flask_test_client.get(
+        f'/tasks/{task_1["id"]}',
+        headers=headers,
+    )
+    assert response.status_code == 401
+
+
+def test_task_put_without_jwt(flask_test_client):
+    user, project = create_user_and_project()
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data)
+    task_1_update_data = {'name': 'test_task_new_name'}
+    response: Response = flask_test_client.put(
+        f'/tasks/{task_1["id"]}',
+        json=task_1_update_data,
+    )
+    assert response.status_code == 401
+    task = db_models.Task.query.first()
+    assert task is not None
+    assert task.name != task_1_update_data['name']
+    assert task.name == task_1_data['name']
+
+
+def test_task_put_with_jwt(flask_test_client):
+    user, project = create_user_and_project()
+    access_token = user.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data)
+    task_1_update_data = {'name': 'test_task_new_name'}
+    response: Response = flask_test_client.put(
+        f'/tasks/{task_1["id"]}',
+        json=task_1_update_data,
+        headers=headers,
+    )
+    assert response.status_code == 200
+    task = db_models.Task.query.first()
+    assert task is not None
+    assert str(task.id) == str(task_1['id'])
+    assert task.name == task_1_update_data['name']
+    assert task.name != task_1_data['name']
+
+
+def test_task_put_with_jwt_not_associated_user(flask_test_client):
+    user, project = create_user_and_project()
+    user2 = create_user(2)
+    access_token = user2.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data)
+    task_1_update_data = {'name': 'test_task_new_name'}
+    response: Response = flask_test_client.put(
+        f'/tasks/{task_1["id"]}',
+        json=task_1_update_data,
+        headers=headers,
+    )
+    assert response.status_code == 401
+    task = db_models.Task.query.first()
+    assert task is not None
+    assert task.name != task_1_update_data['name']
+    assert task.name == task_1_data['name']
+
+
+def test_task_delete_without_jwt(flask_test_client):
+    user, project = create_user_and_project()
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data)
+    response: Response = flask_test_client.delete(
+        f'/tasks/{task_1["id"]}',
+    )
+    assert response.status_code == 401
+    assert db_models.Task.query.count() == 1
+
+
+def test_task_delete_with_jwt(flask_test_client):
+    user, project = create_user_and_project()
+    access_token = user.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data)
+    response: Response = flask_test_client.delete(
+        f'/tasks/{task_1["id"]}',
+        headers=headers,
+    )
+    data = response.get_json()
+    assert response.status_code == 200
+    assert data['id'] == str(task_1['id'])
+    assert db_models.Task.query.count() == 0
+
+
+def test_task_delete_with_jwt_not_associated_user(flask_test_client):
+    user, project = create_user_and_project()
+    user2 = create_user(2)
+    access_token = user2.create_access_token()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    task_group_data_1 = {'name': 'test_task_group_1', 'project_id': project['id']}
+    task_group_1 = db_services.TaskGroupService.create(task_group_data_1)
+    task_1_data = {
+        'name': 'test_task',
+        'task_group_id': task_group_1['id'],
+        'deadline': '2020-12-28T11:59:22.490Z',
+        'user_id': user['id'],
+    }
+    task_1 = db_services.TaskService.create(task_1_data)
+    response: Response = flask_test_client.delete(
+        f'/tasks/{task_1["id"]}',
+        headers=headers,
+    )
+    assert response.status_code == 401
+    assert db_models.Task.query.count() == 1
