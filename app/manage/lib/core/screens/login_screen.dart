609c27bf540cc587a548ebab9813a27a42d41fab
repelliuss <repelliuss@gmail.com>
2020-12-28 03:38:44 +0300

    Modify login screen to use login controller

--- app/manage/lib/core/screens/login_screen.dart
+++ app/manage/lib/core/screens/login_screen.dart
@@ -1,130 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:manage/core/model/user.dart';
-import 'package:manage/core/router/manage_route.dart';
-import 'package:manage/core/router/manage_route_state.dart';
+import 'package:manage/core/controller/login_controller.dart';
 import 'package:provider/provider.dart';
-import 'package:manage/core/service/user_service.dart' as service;
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController _mailCont;
-  TextEditingController _passCont;
-  bool _validMail = true;
-  bool _validPass = true;
-  bool _userExist = true;
-
-  void initState() {
-    super.initState();
-    _mailCont = TextEditingController();
-    _passCont = TextEditingController();
-  }
-
-  void dispose() {
-    _mailCont.dispose();
-    _passCont.dispose();
-    super.dispose();
-  }
-
-  Future<void> login(String mail, String password) async {
-    _userExist = await service.login(User(email: mail, password: password));
-  }
-
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text('Login'),
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(
-                height: 40.0,
-              ),
-              TextField(
-                controller: _mailCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'email',
-                  errorText: _validMail ? null : 'You must fill here',
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextField(
-                obscureText: true,
-                controller: _passCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'password',
-                  errorText: _validPass ? null : 'You must fill here',
-                ),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Center(
-                child: Text(
-                  _userExist ? '' : 'Wrong information',
-                  style: TextStyle(color: Colors.red),
-                ),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  RaisedButton(
-                      child: Text('Login'),
-                      onPressed: () {
-                        setState(() {
-                          _mailCont.text.isEmpty
-                              ? _validMail = false
-                              : _validMail = true;
-                          _passCont.text.isEmpty
-                              ? _validPass = false
-                              : _validPass = true;
-                        });
-                        if (_validMail == true && _validPass == true) {
-                          login(_mailCont.text, _passCont.text);
-                          if (_userExist) {
-                            context
-                                .read<ManageRouteState>()
-                                .update(ManageRoute.teams);
-                          }
-                        }
-                      }),
-                  SizedBox(
-                    width: 5.0,
-                  ),
-                  RaisedButton(
-                    child: Text('Sign up'),
-                    onPressed: () {
-                      setState(() {
-                        _mailCont.clear();
-                        _passCont.clear();
-                      });
-                      context
-                          .read<ManageRouteState>()
-                          .update(ManageRoute.signup);
-                    },
-                  )
-                ],
-              )
-            ],
+          child: ChangeNotifierProvider(
+            create: (_) => LoginController(),
+            child: LoginForm()
           ),
         ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<LoginController>(
+      builder: (context, controller, child) => Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 40.0,
+          ),
+          TextField(
+            controller: controller.email,
+            cursorColor: Theme.of(context).colorScheme.secondaryVariant,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'email',
+              errorText: controller.emailError,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          TextField(
+            obscureText: true,
+            controller: controller.password,
+            cursorColor: Theme.of(context).colorScheme.secondaryVariant,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'password',
+              errorText: controller.passwordError,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Center(
+            child: Text(
+              controller.credentialsError,
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Column(
+            children: [
+              RaisedButton(
+                  child: Text('Login'),
+                  onPressed: () {
+                    controller.onLogin(context);
+                  }
+                ),
+              SizedBox(
+                width: 5.0,
+              ),
+              RaisedButton(
+                child: Text('Sign up'),
+                onPressed: () {
+                  controller.onSignUp(context);
+                },
+              )
+            ],
+          )
+        ],
       ),
     );
   }
