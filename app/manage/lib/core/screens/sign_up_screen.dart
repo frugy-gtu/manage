f81ca4f75cbf40d98fe8b3fe0a3ca3aa8c0fef88
repelliuss <repelliuss@gmail.com>
2020-12-28 05:33:41 +0300

    Modify SignUpScreen to use controller

--- app/manage/lib/core/screens/sign_up_screen.dart
+++ app/manage/lib/core/screens/sign_up_screen.dart
@@ -1,60 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:manage/core/model/user.dart';
-import 'package:manage/core/service/response_status.dart';
+import 'package:manage/core/controller/sign_up_controller.dart';
 import 'package:provider/provider.dart';
-import 'package:manage/core/router/manage_route.dart';
-import 'package:manage/core/router/manage_route_state.dart';
-import 'package:manage/core/service/user_service.dart' as service;
-
-class SignUpScreen extends StatefulWidget {
-  @override
-  _SignUpScreenState createState() => _SignUpScreenState();
-}
-
-class _SignUpScreenState extends State<SignUpScreen> {
-  TextEditingController _fNameCont;
-  TextEditingController _lNameCont;
-  TextEditingController _mailCont;
-  TextEditingController _passCont;
-  TextEditingController _uNameCont;
-  bool _validFName = true;
-  bool _validLName = true;
-  bool _validMail = true;
-  bool _validPass = true;
-  bool _registered = true;
-  bool _validUName = true;
-
-  void initState() {
-    super.initState();
-    _fNameCont = TextEditingController();
-    _lNameCont = TextEditingController();
-    _mailCont = TextEditingController();
-    _passCont = TextEditingController();
-    _uNameCont = TextEditingController();
-  }
-
-  void dispose() {
-    _fNameCont.dispose();
-    _lNameCont.dispose();
-    _mailCont.dispose();
-    _passCont.dispose();
-    _uNameCont.dispose();
-    super.dispose();
-  }
-
-  Future<void> register(String username, String mail, String password) async {
-
-    ResponseResult result = await service.signUp(User(
-      username: username,
-      email: mail,
-      password: password,
-    ));
-
-    setState(() {
-      _registered = result.status == Status.success ? true : false;
-    });
-  }
-
+class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,112 +12,78 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(
-                height: 40.0,
-              ),
-              TextField(
-                controller: _fNameCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'first name',
-                  errorText: _validFName ? null : 'You must fill here',
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextField(
-                controller: _lNameCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'last name',
-                  errorText: _validLName ? null : 'You must fill here',
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextField(
-                controller: _uNameCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'username',
-                  errorText: _validUName ? null : 'You must fill here',
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextField(
-                controller: _mailCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'email',
-                  errorText: _validMail ? null : 'You must fill here',
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextField(
-                controller: _passCont,
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'password',
-                  errorText: _validPass ? null : 'You must fill here',
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Center(
-                child: Text(
-                  _registered ? '' : 'Mail address is already used',
-                  style: TextStyle(color: Colors.red),
-                ),
-              ),
-              RaisedButton(
-                child: Text('Sign up'),
-                onPressed: () {
-                  setState(() {
-                    _fNameCont.text.isEmpty
-                        ? _validFName = false
-                        : _validFName = true;
-                    _lNameCont.text.isEmpty
-                        ? _validLName = false
-                        : _validLName = true;
-                    _uNameCont.text.isEmpty
-                        ? _validUName = false
-                        : _validUName = true;
-                    _mailCont.text.isEmpty
-                        ? _validMail = false
-                        : _validMail = true;
-                    _passCont.text.isEmpty
-                        ? _validPass = false
-                        : _validPass = true;
-                  });
-                  if (_validFName == true &&
-                      _validLName == true &&
-                      _validUName == true &&
-                      _validMail == true &&
-                      _validPass == true) {
-                    register(_uNameCont.text, _mailCont.text, _passCont.text);
-                    if (_registered) {
-                      context
-                          .read<ManageRouteState>()
-                          .update(ManageRoute.login);
-                    }
-                  }
-                },
-              ),
-            ],
+          child: ChangeNotifierProvider(
+            create: (_) => SignUpController(),
+            child: SignUpForm(),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SignUpForm extends StatelessWidget {
+  const SignUpForm({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<SignUpController>(
+      builder: (context, controller, child) => Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 40.0,
+          ),
+          TextField(
+            controller: controller.uName,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'username',
+              errorText: controller.uNameError,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          TextField(
+            controller: controller.email,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'email',
+              errorText: controller.emailError,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          TextField(
+            controller: controller.password,
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'password',
+              errorText: controller.passwordError,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Center(
+            child: Text(
+              controller.credentialsError,
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+          RaisedButton(
+            child: Text('Sign up'),
+            onPressed: () {
+              controller.onSignUp(context);
+            },
+          ),
+        ],
       ),
     );
   }
