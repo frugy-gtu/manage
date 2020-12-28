b580b9a32eddc4296842c35e728c92b5ae24a3f6
repelliuss <repelliuss@gmail.com>
2020-12-28 20:39:48 +0300

    Update services with new manage service

--- app/manage/lib/core/service/team_service.dart
+++ app/manage/lib/core/service/team_service.dart
@@ -1,41 +1,17 @@
 import 'package:manage/core/model/team_model.dart';
 import 'package:manage/core/model/team_create_model.dart';
+import 'package:manage/core/service/request_method.dart';
 import 'package:manage/core/service/response_status.dart';
-import 'package:manage/core/model/error_model.dart';
 
-import 'manage_response.dart';
 import 'manage_service.dart' as service;
 
-Future<ResponseResult<List<TeamModel>>> teams() async {
-  ManageResponse response = await service.get('/teams/');
+Future<RequestResult<List<TeamModel>>> teams() async =>
+    (await service.request<TeamModel>(
+      method: RequestMethod.get,
+      url: '/teams/',
+      decode: (i) => TeamModel.fromJson(i),
+    ))
+        .castTo<List<TeamModel>>();
 
-  if (response == null) {
-    return ResponseResult(Status.fail);
-  }
-
-  if (response.success == null) {
-    return ResponseResult(Status.fail, msg: response.fail.data);
-  }
-
-  return ResponseResult<List<TeamModel>>(Status.success,
-      data: (response.success.data as List)
-          .map((i) => TeamModel.fromJson(i))
-          .toList());
-}
-
-Future<ResponseResult> createTeam(TeamCreateModel request) async {
-  ManageResponse response =
-      await service.post('/teams/', data: request.toJson());
-
-  if (response == null) {
-    return ResponseResult(Status.fail);
-  }
-
-  if (response.success == null) {
-    ErrorModel error = ErrorModel.fromJson(response.fail.data);
-    String errorMsg = error?.table['schema_errors']?.values?.first[0];
-    return ResponseResult(Status.fail, msg: errorMsg ?? 'Something went wrong.');
-  }
-
-  return ResponseResult(Status.success);
-}
+Future<RequestResult> createTeam(TeamCreateModel model) async => await service
+    .request(method: RequestMethod.post, url: '/teams/', jsonData: model.toJson());
--- app/manage/lib/core/service/user_service.dart
+++ app/manage/lib/core/service/user_service.dart
@@ -1,45 +1,22 @@
 import 'package:manage/core/cache/auth.dart';
 import 'package:manage/core/model/access_model.dart';
-import 'package:manage/core/model/error_model.dart';
 import 'package:manage/core/model/user_model.dart';
-import 'package:manage/core/service/manage_response.dart';
+import 'package:manage/core/service/request_method.dart';
 import 'package:manage/core/service/response_status.dart';
 
 import 'manage_service.dart' as service;
 
-Future<ResponseResult> login(UserModel request) async {
-  ManageResponse response =
-      await service.post('/users/login', data: request.toJson());
+Future<RequestResult> login(UserModel model) async => service.request(
+    method: RequestMethod.post,
+    url: '/users/login',
+    jsonData: model.toJson(),
+    successCallback: (success) {
+      Auth.accessToken = AccessModel.fromJson(success.data).accessToken;
+      Auth.status = AuthStatus.logged_in;
+    });
 
-  if (response == null) {
-    return ResponseResult(Status.fail);
-  }
-
-  if (response.success == null) {
-    ErrorModel error = ErrorModel.fromJson(response.fail.data);
-    String errorMsg = error?.table['schema_errors']?.values?.first[0];
-    return ResponseResult(Status.fail, msg: errorMsg ?? 'Something went wrong.');
-  }
-
-  Auth.accessToken = AccessModel.fromJson(response.success.data).accessToken;
-  Auth.status = AuthStatus.logged_in;
-
-  return ResponseResult(Status.success);
-}
-
-Future<ResponseResult> signUp(UserModel request) async {
-  ManageResponse response =
-      await service.post('/users/signup', data: request.toJson());
-
-  if (response == null) {
-    return ResponseResult(Status.fail);
-  }
-
-  if (response.success == null) {
-    ErrorModel error = ErrorModel.fromJson(response.fail.data);
-    String errorMsg = error?.table['schema_errors']?.values?.first[0];
-    return ResponseResult(Status.fail, msg: errorMsg ?? 'Something went wrong');
-  }
-
-  return ResponseResult(Status.success);
-}
+Future<RequestResult> signUp(UserModel model) async => service.request(
+      method: RequestMethod.post,
+      url: '/users/signup',
+      jsonData: model.toJson(),
+    );
