5a602e07bb33a09ac1f3c5ca0e7ab06f6d2ebf8b
repelliuss <repelliuss@gmail.com>
2020-12-28 04:42:41 +0300

    Update manage service to use manage response

--- app/manage/lib/core/service/manage_service.dart
+++ app/manage/lib/core/service/manage_service.dart
@@ -1,12 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:manage/core/cache/auth.dart';
+import 'package:manage/core/service/manage_response.dart';
 
 String _baseUrl = 'http://ec2-52-15-131-192.us-east-2.compute.amazonaws.com';
 
-Future<Response> post(String url,
+Future<ManageResponse> post(String url,
     {Map<String, dynamic> data, Map<String, dynamic> queryParameters}) async {
   try {
-    return await Dio().post(
+    Response response = await Dio().post(
       _baseUrl + url,
       data: data,
       queryParameters: queryParameters,
@@ -16,10 +17,12 @@ Future<Response> post(String url,
         },
       ),
     );
+
+    return ManageResponse(response, null);
   } on DioError catch (e) {
     print(e);
     print(e.response.data);
-    return null;
+    return ManageResponse(null, e.response);
   } catch (e) {
     print(e);
     return null;
