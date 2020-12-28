b22a08eada995e5b7f2fd80523bb6ec4de179ebb
repelliuss <repelliuss@gmail.com>
2020-12-28 14:31:16 +0300

    Add createTeam service

--- app/manage/lib/core/service/team_service.dart
+++ app/manage/lib/core/service/team_service.dart
@@ -1,5 +1,7 @@
 import 'package:manage/core/model/team.dart';
+import 'package:manage/core/model/team_create_model.dart';
 import 'package:manage/core/service/response_status.dart';
+import 'package:manage/core/model/error.dart';
 
 import 'manage_response.dart';
 import 'manage_service.dart' as service;
@@ -19,4 +21,21 @@ Future<ResponseResult<List<Team>>> teams() async {
       data: (response.success.data as List)
           .map((i) => Team.fromJson(i))
           .toList());
+}
+
+Future<ResponseResult> createTeam(TeamCreateModel request) async {
+  ManageResponse response =
+      await service.post('/teams/', data: request.toJson());
+
+  if (response == null) {
+    return ResponseResult(Status.fail);
+  }
+
+  if (response.success == null) {
+    Error error = Error.fromJson(response.fail.data);
+    String errorMsg = error?.table['schema_errors']?.values?.first[0];
+    return ResponseResult(Status.fail, msg: errorMsg ?? 'Something went wrong.');
+  }
+
+  return ResponseResult(Status.success);
 }
