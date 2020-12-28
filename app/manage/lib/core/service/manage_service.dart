ffe6bfb0b2b7469b7ba09605f54230c9d3dcb09f
repelliuss <repelliuss@gmail.com>
2020-12-28 03:37:33 +0300

    Handle errors on post request

--- app/manage/lib/core/service/manage_service.dart
+++ app/manage/lib/core/service/manage_service.dart
@@ -6,14 +6,20 @@ String _baseUrl = 'http://ec2-52-15-131-192.us-east-2.compute.amazonaws.com';
 Future<Response> post(String url,
     {Map<String, dynamic> data, Map<String, dynamic> queryParameters}) async {
   try {
-    return await Dio().post(_baseUrl + url,
-        data: data,
-        queryParameters: queryParameters,
-        options: Options(
-          headers: <String, dynamic>{
-            'Authorization': 'Bearer ' + Auth.accessToken
-          },
-        ));
+    return await Dio().post(
+      _baseUrl + url,
+      data: data,
+      queryParameters: queryParameters,
+      options: Options(
+        headers: <String, dynamic>{
+          'Authorization': 'Bearer ' + Auth.accessToken
+        },
+      ),
+    );
+  } on DioError catch (e) {
+    print(e);
+    print(e.response.data);
+    return null;
   } catch (e) {
     print(e);
     return null;
