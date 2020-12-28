9da3bb2449e38b19115d992a506f42975347cd23
repelliuss <repelliuss@gmail.com>
2020-12-28 03:36:11 +0300

    Add access model

---
+++ app/manage/lib/core/model/access.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'access.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Access _$AccessFromJson(Map<String, dynamic> json) {
+  return Access(
+    accessToken: json['access_token'] as String,
+  );
+}
+
+Map<String, dynamic> _$AccessToJson(Access instance) => <String, dynamic>{
+      'access_token': instance.accessToken,
+    };
