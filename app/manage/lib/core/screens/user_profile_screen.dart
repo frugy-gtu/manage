537c8efaadb17a55cc2659a33111966a7797107d
Yusuf Fatih Şişman <62207089+YusufFatihSisman@users.noreply.github.com>
2020-12-30 00:54:56 +0200

    User profile screen changes
    User profile screen no longer shows projects and teams

--- app/manage/lib/core/screens/user_profile_screen.dart
+++ app/manage/lib/core/screens/user_profile_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'Models/user_profile_model.dart';
 
 class UserPage extends StatelessWidget {
   
-  final UserProfile user;
+  final UserProfileModel user;
 
   UserPage({@required this.user});
 
@@ -14,187 +15,87 @@ class UserPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
         iconTheme: IconThemeData(color: Colors.black),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ProfileInfos(user: user),
-              SizedBox(height: 20.0),
-              Text('Projects:'),
-              ProjectsList(user: user),
-              Text('Teams:'),
-              TeamsList(user: user),
-            ],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.do_not_disturb),
+            onPressed: (){
+              //logout
+            }
           ),
-        ),
+        ],
       ),
-      drawer: Theme(
-        data: Theme.of(context).copyWith(
-          canvasColor: Colors.black,
-        ),
-        child: SizedBox(
-          width: 150.0,
-          child: Drawer(
-            child: ListView(
-              children: [
-                ListTile(
-                  title: Text('Back', style: TextStyle(color: Colors.white)),
-                  onTap: (){
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  title: Text('Agenda', style: TextStyle(color: Colors.white)),
-                  onTap: (){
-                    //Navigator.popAndPushNamed(context, '/agendaPage');
-                  },
-                ),
-                ListTile(
-                  title: Text('Settings', style: TextStyle(color: Colors.white)),
-                  onTap: (){
-                    //Navigator.popAndPushNamed(context, '/settingsPage');
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: ProfileInfos(user: user,),
+      drawer: ThisDrawer(),
     );
   }
 }
 
 class ProfileInfos extends StatelessWidget {
-  final UserProfile user;
+  final UserProfileModel user;
 
   ProfileInfos({this.user});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: FutureBuilder(
-          future: Future.wait([user.getPhoto(), user.getName(), user.getLName()]),
-          builder: (context, user){
-            if(user.hasData){
-              Icon photo = Icon(user.data[0], size:100.0);
-              String name = user.data[1];
-              String lName = user.data[2];
-              return Column(
-                children: [
-                  photo,
-                  Center(child: Text(name, style: TextStyle(fontSize: 30.0))),
-                  Center(child: Text(lName, style: TextStyle(fontSize: 30.0))),
-                ],
-              );
-            }else{
-              return Center(
-                child: Text('Could not access user infos', style: TextStyle(fontSize: 30))
-              );
-            }
-          }
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 150.0, 
+              width: 150.0, 
+              child: user.profilePhoto,
+            ),
+            SizedBox(height: 5.0,),
+            Text(user.userName, style: TextStyle(fontSize: 30.0)),
+            SizedBox(height: 5.0,),
+            Text(user.name, style: TextStyle(fontSize: 30.0)),
+            SizedBox(height: 5.0,),
+            Text(user.surname, style: TextStyle(fontSize: 30.0)),
+          ],
         ),
-      )
+      ),
     );
-  }
+  }      
 }
 
-class TeamsList extends StatelessWidget {
-  final UserProfile user;
-
-  TeamsList({this.user});
-
+class ThisDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100.0,
-      child: FutureBuilder<List<Team>>(
-        future: user.getTeams(),
-        builder: (context, teams){
-          if(teams.hasData){
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: teams.data.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, int position){
-                final currentTeam = teams.data[position];
-                return GestureDetector(
-                  onTap: () {
-                    //go to team page
-                  },
-                  child: Card(
-                    margin: EdgeInsets.all(5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(currentTeam.getName()),
-                        currentTeam.getIcon(),
-                      ],
-                    ),
-                  ),
-                );
-              }
-            );
-          }else{
-            return Center(
-              child: Text('No Teams', style: TextStyle(fontSize: 30),),
-            );
-          }
-        }
-      )  
-    );
-  }
-}
-
-class ProjectsList extends StatelessWidget {
-  final UserProfile user;
-
-  ProjectsList({this.user});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100.0,
-      child: FutureBuilder<List<Project>>(
-        future: user.getProjects(),
-        builder: (context, projects){
-          if(projects.hasData){
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: projects.data.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, int position){
-                final currentProject = projects.data[position];
-                return GestureDetector(
-                  onTap: () {
-                    //go to project page
-                  },
-                  child: Card(
-                    margin: EdgeInsets.all(5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(currentProject.getName()),
-                        currentProject.getIcon(),
-                      ],
-                    ),
-                  ),
-                );
-              }
-            );
-          }else{
-            return Center(
-              child: Text('No Projects', style: TextStyle(fontSize: 30),),
-            );
-          }
-        }
-      )  
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Colors.black,
+      ),
+      child: SizedBox(
+        width: 150.0,
+        child: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                title: Text('Back', style: TextStyle(color: Colors.white)),
+                onTap: (){
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Agenda', style: TextStyle(color: Colors.white)),
+                onTap: (){
+                  //Navigator.popAndPushNamed(context, '/agendaPage');
+                },
+              ),
+              ListTile(
+                title: Text('Settings', style: TextStyle(color: Colors.white)),
+                onTap: (){
+                  //Navigator.popAndPushNamed(context, '/settingsPage');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
