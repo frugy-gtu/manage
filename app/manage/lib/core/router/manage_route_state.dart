07e36cf33d7c0dedb69a77727be6ee95ee9260ae
repelliuss <repelliuss@gmail.com>
2021-01-01 19:18:48 +0300

    Add member and user to route state

--- app/manage/lib/core/router/manage_route_state.dart
+++ app/manage/lib/core/router/manage_route_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:manage/core/model/team_user_model.dart';
 import 'package:manage/core/router/manage_route.dart';
 
 import '../model/team_model.dart';
@@ -6,6 +7,8 @@ import '../model/team_model.dart';
 class ManageRouteState extends ChangeNotifier {
   ManageRoute _route;
   TeamModel _team;
+  TeamUserModel _member;
+  TeamUserModel user;
 
   ManageRouteState() : _route = ManageRoute.teams;
 
@@ -13,12 +16,19 @@ class ManageRouteState extends ChangeNotifier {
 
   TeamModel get team => _team;
 
-  void update(ManageRoute route, {TeamModel team}) {
+  TeamUserModel get member => _member;
+
+  void update(ManageRoute route, {TeamModel team, TeamUserModel member,}) {
     assert(route != null);
 
     if(route == ManageRoute.team) {
       assert(team != null);
       _team = team;
+    }
+
+    if(route == ManageRoute.member_profile) {
+      assert(member != null);
+      _member = member;
     }
 
     _route = route;
