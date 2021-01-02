895fddc410ee090cd78e2645bdfc403cc2ca85f4
ismltpn <ismltpn@gmail.com>
2021-01-02 16:50:03 +0300

    Create profile routes and update existing routes

--- api/manage-api/manage_api/routes/users/schemas/request.py
+++ api/manage-api/manage_api/routes/users/schemas/request.py
@@ -1,3 +1,5 @@
+from flask import abort
+from flask.globals import request
 from marshmallow import (
     fields,
     Schema,
@@ -6,6 +8,7 @@ from marshmallow import (
     validates_schema,
     ValidationError,
 )
+from manage_api.routes.common_schemas import JWTSchema
 from manage_api.db.services import UserService
 
 
@@ -20,9 +23,11 @@ class LoginSchema(Schema):
             if data.get('email'):
                 if data.get('username'):
                     raise ValidationError('Don\'t user both username and email')
-                user = UserService(email=data['email'])
+                user = UserService(email=data['email'], joined_relations=['profile'])
             elif data.get('username'):
-                user = UserService(username=data['username'])
+                user = UserService(
+                    username=data['username'], joined_relations=['profile']
+                )
             else:
                 raise ValidationError('Provide an email or username')
         except ValueError as e:
@@ -31,13 +36,16 @@ class LoginSchema(Schema):
             if user.check_password(data['password']):
                 data['user'] = user
                 return data
-            raise ValidationError('Wrong Password')
+            raise abort(401, 'Wrong Password')
 
 
 class SignupSchema(Schema):
     username = fields.String(required=True, allow_none=False)
     email = fields.Email(required=True, allow_none=False)
     password = fields.String(required=True, allow_none=False)
+    name = fields.String(required=False, allow_none=True)
+    surname = fields.String(required=False, allow_none=True)
+    avatar = fields.String(required=False, allow_none=True)
 
     @validates_schema
     def check_uniqueness_of_user(self, data, **kwargs):
@@ -50,6 +58,49 @@ class SignupSchema(Schema):
                 raise ValidationError('User with this email exists')
             except ValueError:
                 pass
+
+    @post_load
+    def post_load(self, data, **kwargs):
+        profile_keys = ['name', 'surname', 'avatar']
+        data['profile'] = {}
+        for k in profile_keys:
+            v = data.get(k)
+            if v:
+                data['profile'][k] = v
+                del data[k]
+        return data
+
+
+class UserProfileGetSchema(JWTSchema):
+    @post_load
+    def post_load(self, data, **kwargs):
+        user_id = self.get_user_id()
+        data['user'] = UserService(id=user_id, joined_relations=['profile'])
+        return data
+
+
+class UserProfilePutSchema(JWTSchema):
+    name = fields.String(required=False, allow_none=True)
+    surname = fields.String(required=False, allow_none=True)
+    avatar = fields.String(required=False, allow_none=True)
+
+    @post_load
+    def post_load(self, data, **kwargs):
+        user_id = self.get_user_id()
+        data['user'] = UserService(id=user_id, joined_relations=['profile'])
+        return data
+
+
+class UserOtherProfileGetSchema(JWTSchema):
+    @post_load
+    def post_load(self, data, **kwargs):
+        try:
+            data['user'] = UserService(
+                id=request.view_args['user_id'], joined_relations=['profile']
+            )
+            return data
+        except ValueError as e:
+            abort(404, str(e))
 
 
 class ActivateSchema(Schema):
