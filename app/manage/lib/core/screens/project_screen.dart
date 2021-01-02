f42f35e3790a4d1469bcad248e3399d577884f8e
repelliuss <repelliuss@gmail.com>
2021-01-02 05:10:29 +0300

    Create project screen controller

---
+++ app/manage/lib/core/screens/project_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:manage/core/model/team_project_model.dart';
+
+class ProjectScreen extends StatelessWidget {
+  final TeamProjectModel project;
+
+  const ProjectScreen(this.project);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
