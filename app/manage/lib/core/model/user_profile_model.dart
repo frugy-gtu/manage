2734df8926c8abb0aeb6886908c21e34cbbbcf1f
repelliuss <repelliuss@gmail.com>
2021-01-03 23:00:52 +0300

    Add serialization to user profile model

--- app/manage/lib/core/model/user_profile_model.dart
+++ app/manage/lib/core/model/user_profile_model.dart
@@ -1,13 +1,20 @@
-import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:manage/core/model/manage_model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-class UserProfileModel extends ManageModel{
-  final String userName;
+part 'user_profile_model.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
+class UserProfileModel extends ManageModel {
   final String name;
   final String surname;
-  final Image profilePhoto;
+  final String avatar;
 
-  const UserProfileModel({@required this.userName, @required this.name, @required this.surname, this.profilePhoto});
+  const UserProfileModel(
+      {@required this.name, @required this.surname, this.avatar});
 
+  factory UserProfileModel.fromJson(Map<String, dynamic> json) =>
+      _$UserProfileModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserProfileModelToJson(this);
 }
