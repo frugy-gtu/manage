57706621e1b83878af456d8e63db4ff116d84f60
repelliuss <repelliuss@gmail.com>
2021-01-03 04:57:41 +0300

    Remove app bar buttons on teams screen

--- app/manage/lib/core/screens/teams_screen.dart
+++ app/manage/lib/core/screens/teams_screen.dart
@@ -15,24 +15,7 @@ class TeamsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu_rounded),
-          enableFeedback: false,
-          onPressed: () {},
-          splashRadius: 20,
-        ),
         title: const Text('Teams'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.account_circle),
-            enableFeedback: false,
-            onPressed: () {
-              context.read<ManageRouteState>().update(ManageRoute.user_profile,
-                  prevUserProfileRoute: ManageRoute.teams);
-            },
-            splashRadius: 20,
-          ),
-        ],
       ),
       body: FutureBuilder(
         future: controller.teams(),
