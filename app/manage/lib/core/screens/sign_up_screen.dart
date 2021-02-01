161fdba66b94cbc6ecd4e796d565fd908633b4a9
repelliuss <repelliuss@gmail.com>
2021-02-01 12:01:01 +0300

    Add scroll to sign up

--- app/manage/lib/core/screens/sign_up_screen.dart
+++ app/manage/lib/core/screens/sign_up_screen.dart
@@ -7,20 +7,18 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text('Manage', style: TextStyle(color: Theme.of(context).colorScheme.secondary),),
-        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+        title: Text(
+          'Manage',
+          style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+        ),
+        iconTheme:
+            IconThemeData(color: Theme.of(context).colorScheme.secondary),
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) => Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: constraints.maxWidth / 9,
-          ),
-          child: ChangeNotifierProvider(
-            create: (_) => SignUpScreenController(),
-            child: SignUpForm(),
-          ),
+      body: ChangeNotifierProvider(
+        create: (_) => SignUpScreenController(),
+        child: SingleChildScrollView(
+          child: SignUpForm(),
         ),
       ),
     );
@@ -34,121 +32,120 @@ class SignUpForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) => Padding(
-        padding: EdgeInsets.only(top: constraints.maxHeight / 28),
-        child: Consumer<SignUpScreenController>(
-            builder: (context, controller, child) => Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+    return Consumer<SignUpScreenController>(
+      builder: (context, controller, child) => Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 50,
+          horizontal: 50,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Icon(Icons.person),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: TextField(
-                        controller: controller.uName,
-                        cursorColor: Theme.of(context).colorScheme.secondaryVariant,
-                        decoration: InputDecoration(
-                          labelText: 'username',
-                          errorText: controller.uNameError,
-                        ),
-                      ),
-                    ),
-                  ],
+                Icon(Icons.person),
+                SizedBox(
+                  width: 10,
                 ),
-                Row(
-                  children: [
-                    Icon(Icons.text_fields),
-                    SizedBox(
-                      width: 10,
+                Expanded(
+                  child: TextField(
+                    controller: controller.uName,
+                    cursorColor: Theme.of(context).colorScheme.secondaryVariant,
+                    decoration: InputDecoration(
+                      labelText: 'username',
+                      errorText: controller.uNameError,
                     ),
-                    Expanded(
-                      child: TextField(
-                        controller: controller.fName,
-                        cursorColor: Theme.of(context).colorScheme.secondaryVariant,
-                        decoration: InputDecoration(
-                          labelText: 'name',
-                          errorText: controller.fNameError,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.text_fields),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: TextField(
-                        controller: controller.lName,
-                        cursorColor: Theme.of(context).colorScheme.secondaryVariant,
-                        decoration: InputDecoration(
-                          labelText: 'surname',
-                          errorText: controller.lNameError,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.mail),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: TextField(
-                        controller: controller.email,
-                        cursorColor: Theme.of(context).colorScheme.secondaryVariant,
-                        decoration: InputDecoration(
-                          labelText: 'email',
-                          errorText: controller.emailError,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                  Icon(Icons.lock),
-                  SizedBox(
-                    width: 10,
                   ),
-                    Expanded(
-                      child: TextField(
-                        controller: controller.password,
-                        cursorColor: Theme.of(context).colorScheme.secondaryVariant,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'password',
-                          errorText: controller.passwordError,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Text(
-                  controller.credentialsError,
-                  style: TextStyle(color: Colors.red),
-                ),
-                WideCardButton(
-                  child: Text(
-                    'Sign Up',
-                    style: Theme.of(context)
-                        .textTheme
-                        .button,
-                  ),
-                  onTap: () {
-                    controller.onSignUp(context);
-                  },
                 ),
               ],
             ),
+            Row(
+              children: [
+                Icon(Icons.text_fields),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: controller.fName,
+                    cursorColor: Theme.of(context).colorScheme.secondaryVariant,
+                    decoration: InputDecoration(
+                      labelText: 'name',
+                      errorText: controller.fNameError,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.text_fields),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: controller.lName,
+                    cursorColor: Theme.of(context).colorScheme.secondaryVariant,
+                    decoration: InputDecoration(
+                      labelText: 'surname',
+                      errorText: controller.lNameError,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.mail),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: controller.email,
+                    cursorColor: Theme.of(context).colorScheme.secondaryVariant,
+                    decoration: InputDecoration(
+                      labelText: 'email',
+                      errorText: controller.emailError,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.lock),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: controller.password,
+                    cursorColor: Theme.of(context).colorScheme.secondaryVariant,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'password',
+                      errorText: controller.passwordError,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              controller.credentialsError,
+              style: TextStyle(color: Colors.red),
+            ),
+            WideCardButton(
+              child: Text(
+                'Sign Up',
+                style: Theme.of(context).textTheme.button,
+              ),
+              onTap: () {
+                controller.onSignUp(context);
+              },
+            ),
+          ],
         ),
       ),
     );
